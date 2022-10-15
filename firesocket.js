b0b4f2057f310f01171851fd7d2f0a0080a5a97d
blackkong8 <blackkong8@gmail.com>
2022-10-15 22:03:25 +0900

    first commit

---
+++ firesocket.js
@@ -0,0 +1,33 @@
+'use strict'
+
+import { update, onValue } from 'https://www.gstatic.com/firebasejs/9.11.0/firebase-database.js'
+
+export class FireSocket {
+    //---Event---
+    onclose = () => { }
+    onerror = () => { }
+    onmessage = () => { }
+    onopen = () => { }
+
+    //---Method---
+    close() {
+        this.onclose()
+    }
+
+    send(data) {
+        update(this.reference, { data: data })
+            .catch((error) => this.onerror(error))
+    }
+
+    //---***---
+    constructor(reference) {
+        this.reference = reference
+
+        onValue(this.reference, (snapshot) => {
+            const data = snapshot.val()
+            this.onmessage(data)
+        })
+
+        this.onopen()
+    }
+}
